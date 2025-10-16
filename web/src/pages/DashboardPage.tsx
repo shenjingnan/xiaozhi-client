fb3a956ada4dbe9160dcd87042b1cac60302c9b3
shenjingnan <sjn.code@gmail.com>
2025-10-16 19:06:43 +0800

    WIP

--- web/src/pages/DashboardPage.tsx
+++ web/src/pages/DashboardPage.tsx
@@ -2,7 +2,6 @@ import { AppSidebar } from "@/components/AppSidebar";
 import { DashboardStatusCard } from "@/components/DashboardStatusCard";
 import { McpServerList } from "@/components/McpServerList";
 import { SiteHeader } from "@/components/SiteHeder";
-import { VersionUpgradeDialog } from "@/components/VersionUpgradeDialog";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { useWebSocketActions } from "@/providers/WebSocketProvider";
 
@@ -18,9 +17,6 @@ export default function DashboardPage() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <div className="px-4 md:px-6">
-                <VersionUpgradeDialog defaultSelectedVersion="1.7.5" />
-              </div>
               <DashboardStatusCard />
               <McpServerList updateConfig={updateConfig} />
             </div>
