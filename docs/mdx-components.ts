4477a945497c33bd074abd94de58aa5a916b7453
shenjingnan <sjn.code@gmail.com>
2025-12-24 17:15:49 +0800

    WIP

--- docs/mdx-components.ts
+++ docs/mdx-components.ts
@@ -2,7 +2,7 @@ import { useMDXComponents as getThemeComponents } from "nextra-theme-docs";
 
 const themeComponents = getThemeComponents();
 
-export function useMDXComponents(components: Record<string, any>) {
+export function useMDXComponents(components: Record<string, React.ComponentType<unknown>>) {
   return {
     ...themeComponents,
     ...components,
