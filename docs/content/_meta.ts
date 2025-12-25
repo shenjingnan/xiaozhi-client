9267812660820409b305704e391de057c1e2b293
shenjingnan <sjn.code@gmail.com>
2025-12-25 12:52:00 +0800

    WIP

--- docs/content/_meta.ts
+++ docs/content/_meta.ts
@@ -1,8 +1,8 @@
 import type { MetaRecord } from "nextra";
 
 const meta: MetaRecord = {
-  index: '介绍',
-  "quickstart": "快速上手",
+  index: "介绍",
+  quickstart: "快速上手",
   usage: "使用姿势",
   reference: "参考手册",
 };
