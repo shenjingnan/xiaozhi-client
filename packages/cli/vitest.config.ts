d16f9f0b9d54c581757e9ff659dfb84e751bfc3f
shenjingnan <sjn.code@gmail.com>
2025-12-30 20:13:19 +0800

    WIP

--- packages/cli/vitest.config.ts
+++ packages/cli/vitest.config.ts
@@ -18,9 +18,7 @@ export default defineConfig({
     environment: "node",
     testTimeout: 10000,
     hookTimeout: 10000,
-    include: [
-      resolve(__dirname, "**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"),
-    ],
+    include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     exclude: ["node_modules", "dist"],
     coverage: {
       enabled: true,
